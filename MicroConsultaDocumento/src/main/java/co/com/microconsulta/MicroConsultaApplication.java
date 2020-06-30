e2d6eb28e39dd13a275098c2f04f6d861086b638
fabio <fabio@fabio-HP-Pavilion-g4-Notebook-PC>
2020-06-30 08:30:18 -0500

    Commit

---
+++ MicroConsultaDocumento/src/main/java/co/com/microconsulta/MicroConsultaApplication.java
@@ -0,0 +1,13 @@
+package co.com.microconsulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroConsultaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroConsultaApplication.class, args);
+	}
+
+}
